5bf07e0d2db8be3ac20f288316dc0ea7d719ae77
Syreeta Monroe <syreeta.monroe@gmail.com>
2018-06-22 23:17:56 -0400

    some progress

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+var count = 5;
+var gameTimer;
+
+
+function startTimer() {
+gameTimer = setInterval(function(){
+count--;  
+console.log(count);
+
+
+},1000);
+
+}
+
+function endGame() {
+setTimeout(function(){
+clearInterval(gameTimer);
+count = 5;
+console.log("Game finished");
+
+},5000);
+}
+
+
+
+
+
+$("document").ready(function() {
+  $("#start").on("click", function() {
+    // console.log("test");
+    $("#buttons").hide();
+    $("#questions").show();
+    $("#answers").show();
+    startTimer();
+    endGame();
+  });
+});
