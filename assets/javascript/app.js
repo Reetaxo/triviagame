4ae1f9fbe5ef5f8a7dd9409f33db48ed02e28dea
Syreeta Monroe <syreeta.monroe@gmail.com>
2018-07-30 09:57:06 -0400

    questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,51 @@ console.log(count);
 
 }
 
+function check(){
+
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+  var question3 = document.quiz.question3.value;
+  var question4 = document.quiz.question4.value;
+	var question5 = document.quiz.question5.value;
+  var question6 = document.quiz.question6.value;
+  var question7 = document.quiz.question7.value;
+	var question8 = document.quiz.question8.value;
+	var question9 = document.quiz.question9.value;
+  var question10 = document.quiz.question10.value;
+
+	if (question1 == "John Jay Preston") {
+		correct++;
+}
+	if (question2 == "Heaven on Fifth") {
+		correct++;
+}	
+	if (question3 == "A Pooch") {
+		correct++;
+  }
+  if (question4 == "Her divorce attorney") {
+		correct++;
+}
+	if (question5 == "Blair Underwood") {
+		correct++;
+}	
+	if (question6 == "Samantha Jones") {
+		correct++;
+  }
+  if (question7 == "2") {
+		correct++;
+}
+	if (question8 == "I like my money right where I can see it... hanging in my closet!") {
+		correct++;
+}	
+	if (question9 == "Atlantic City") {
+		correct++;
+  }
+  if (question10 == "Samantha Jones") {
+		correct++;
+	}
+}
+
 function endGame() {
 setTimeout(function(){
 clearInterval(gameTimer);
@@ -19,7 +64,7 @@ count = 5;
 console.log("Game finished");
 
 },5000);
-}
+} 
 
 
 $("document").ready(function() {
@@ -28,7 +73,8 @@ $("document").ready(function() {
     $("#buttons").hide();
     $(".questions").show();
     $("#answers").show();
+    // $("#end").show();
     startTimer();
     endGame();
   });
-});
+})
